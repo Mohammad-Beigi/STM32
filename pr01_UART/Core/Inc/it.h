7f3bc8d30b85b3a8318ff72015d33ad29475c002
Beigi <m.mohammadbeigi28@gmail.com>
2025-05-01 13:07:18 +0330

    First Build

---
+++ pr01_UART/Core/Inc/it.h
@@ -0,0 +1,13 @@
+/*
+ * it.h
+ *
+ *  Created on: May 1, 2025
+ *      Author: Mmoha
+ */
+
+#ifndef INC_IT_H_
+#define INC_IT_H_
+
+
+
+#endif /* INC_IT_H_ */
--- pr01_UART/Core/Src/main.c
+++ pr01_UART/Core/Src/main.c
@@ -5,4 +5,24 @@
  *      Author: Mmoha
  */
 
+// Include
+#include <stm32f1xx_hal.h>
+#include "main.h"
 
+// Function Declaration
+void SystemClockConfig(void);
+
+// Main Function
+int main(void)
+{
+	HAL_Init();
+	SystemClockConfig();
+
+	return 0;
+}
+
+// Function Definitions
+void SystemClockConfig(void)
+{
+
+}

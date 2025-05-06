82e80385c12e112bd505fc6c4f7218f4a4650c08
Beigi <m.mohammadbeigi28@gmail.com>
2025-05-06 20:38:43 +0330

    create 'msp.c' file:

--- pr01_UART/Core/Src/msp.c
+++ pr01_UART/Core/Src/msp.c
@@ -5,4 +5,12 @@
  *      Author: Mmoha
  */
 
+void HAL_MspInit(void)
+{
+	// low level processor specific initialization
+	// 1. Set up the priority grouping
 
+	// 2. Enable the required system exceptions
+
+	// 3. Configure the priority for the system exceptions
+}

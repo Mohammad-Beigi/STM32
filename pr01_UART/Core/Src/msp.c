5111caa25ab14998c07c485090b89e0e87cca4d2
Beigi <m.mohammadbeigi28@gmail.com>
2025-06-11 13:05:14 +0330

    set up the priority grouping

--- pr01_UART/Core/Src/msp.c
+++ pr01_UART/Core/Src/msp.c
@@ -5,10 +5,13 @@
  *      Author: Mmoha
  */
 
+#include "stm32f1xx_hal.h"
+
 void HAL_MspInit(void)
 {
 	// low level processor specific initialization
 	// 1. Set up the priority grouping
+	HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
 
 	// 2. Enable the required system exceptions
 

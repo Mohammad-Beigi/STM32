6f3e1d95f98c342fe670ee37b79506a4634b2842
Beigi <m.mohammadbeigi28@gmail.com>
2025-06-12 17:20:19 +0330

    Configure the priority for the system exeptions.

--- pr01_UART/Core/Src/msp.c
+++ pr01_UART/Core/Src/msp.c
@@ -14,7 +14,10 @@ void HAL_MspInit(void)
 	HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
 
 	// 2. Enable the required system exceptions
-	SCB->SHCSR = 0x7 << 16;		// uage fault, bus fault, mem fault
+	SCB->SHCSR = 0x7 << 16;		// usage fault, bus fault, mem fault
 
 	// 3. Configure the priority for the system exceptions
+	HAL_NVIC_SetPriority(MemoryManagement_IRQn,0,0);
+	HAL_NVIC_SetPriority(BusFault_IRQn,0,0);
+	HAL_NVIC_SetPriority(UsageFault_IRQn,0,0);
 }

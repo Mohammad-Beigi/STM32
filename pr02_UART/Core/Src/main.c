73e8b7dee8f42f9f78d71239db6ec8bbba3b092e
Beigi <m.mohammadbeigi28@gmail.com>
2025-06-12 19:07:54 +0330

    UART2 high level initialization: BaudRate=115200, WordLength=8B, StopBit=1, Parity=None, Mode=RX_TX, HwFlowCtl=None

--- pr02_UART/Core/Src/main.c
+++ pr02_UART/Core/Src/main.c
@@ -12,9 +12,10 @@
 // Function Declaration
 void SystemClockConfig(void);
 void UART2_Init(void);
+void Error_handler(void);
 
 // Global Variable
-
+UART_HandleTypeDef huart2;
 
 // Main Function
 int main(void)
@@ -31,7 +32,25 @@ void SystemClockConfig(void)
 
 }
 
-void UART2_Init()
+void UART2_Init(void)
 {
+	huart2.Instance = USART2;
+	huart2.Init.BaudRate = 115200U;
+	huart2.Init.WordLength = UART_WORDLENGTH_8B;
+	huart2.Init.StopBits = UART_STOPBITS_1;
+	huart2.Init.Parity = UART_PARITY_NONE;
+	huart2.Init.Mode = UART_MODE_TX_RX;
+	huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	if ( HAL_UART_Init(&huart2) != HAL_OK )
+	{
+		Error_handler();
+	}
+}
 
+void Error_handler(void)
+{
+	while(1)
+	{
+
+	}
 }
